02d420679940e56f6ded5d75a63d047de77345bb
Golla Bharath <gollabharath2007@gmail.com>
2025-06-22 18:15:09 +0530

    Got into logic, i have no clue what to do

---
+++ script.js
@@ -0,0 +1,12 @@
+function btnmg(event) {
+  const btn = event.target.textContent;
+  if (!isNaN(parseInt(btn))) {
+    console.log(btn);
+  } else {
+    console.log(btn);
+  }
+}
+
+function firstnum(a) {
+  return parseInt(a);
+}
